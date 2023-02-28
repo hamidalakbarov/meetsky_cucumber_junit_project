8f2a6420211607577fac588c15ebc1f82aea4162
hamidalakbarov <hamidalakbarov@gmail.com>
2023-02-28 12:47:00 -0800

    changing files

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith;
         plugin = {"html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber.json"},
+                },
         publish = true
 )
 
